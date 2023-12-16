b2b56a676bfdab379cda113cdbfde922a3e58524
JoeKaleli <kalelimburu9@gmail.com>
2023-12-16 22:28:08 +0300

    Done with assignment

---
+++ Arrays.js
@@ -0,0 +1,13 @@
+let listOfFruits = ["apple","banana","mango"]
+// for (let i=0;i<listOfFruits.length;i++) {
+// console.log("Start...")
+    // console.log(listOfFruits[i])
+    // console.log("...end")
+// }
+let lenFruits = listOfFruits.length
+let start = 0
+
+while(start < lenFruits) {
+    console.log(listOfFruits[start])
+    start++
+}
--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Code your solution in this file!
+// Code your solution Here !
+function distanceFromHqInBlocks(pickupLocation) {
+  const hqLocation = 50;
+  const distanceFromHqInBlocks = Math.abs(hqLocation - pickupLocation);
+  return distanceFromHqInBlocks;
+}
+console.log(`${distanceFromHqInBlocks(42)}`)
+
+
+
+function distanceFromHqInFeet(pickupLocation) {
+  const distanceInBlocks = distanceFromHqInBlocks(pickupLocation);
+  const distanceFromHqInFeet = Math.abs(distanceInBlocks*264);
+  return distanceFromHqInFeet ;
+}
+console.log(`${distanceFromHqInFeet(42)}`)
+
+function distanceTravelledInFeet(start,destination) {
+const distanceInFeet = Math.abs((start - destination)*264);
+return distanceInFeet;
+}
+console.log(`${distanceTravelledInFeet(45,12)}`)
+
+function calculatesFarePrice(start,destination) {
+const distance = Math.abs(destination-start);
+
+if (distance<=400) {
+  return 0;
+} 
+else if (distance <= 2000) 
+{
+  return (distance - 400)*0.02;
+} 
+else if (distance < 2500) 
+{
+return 25;
+} 
+else 
+{
+  return 'cannot travel that far'
+}
+}
+console.log(`${calculatesFarePrice(927,27)}`)
+ 
